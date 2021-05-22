f28bb206905ad04035a3be17eef52f1736ce8ea0
Mostafa Kassem <moustafakassem12@gmail.com>
2021-05-23 00:43:23 +0300

    Step 8

---
+++ Basics-02/Step8.php
@@ -0,0 +1,14 @@
+<?php
+
+function isPowerOfTwo($n)
+{
+    if (($n & ($n - 1)) == 0) {
+        return "$n is power of 2";
+    } else {
+        return "$n is not power of 2";
+    }
+}
+
+echo(isPowerOfTwo(4) . "\n");
+
+?>
