d5bf9842637c07ffe4cbcadfb2dca975aa3946cb
Mostafa Kassem <moustafakassem12@gmail.com>
2021-05-22 19:33:14 +0300

    Step 4

---
+++ Step4.php
@@ -0,0 +1,16 @@
+
+<?php
+
+$name = 'Mostafa';
+$position = 'Web Developer';
+$github_url = 'https://github.com/';
+
+echo "Hello, I'm " . $name . ", I'm a " . $position . " please check my github link <br/>" . $github_url;
+echo "<br /><br />";
+echo "Hello, I'm $name, I'm a $position please check my github link <br/>$github_url";
+echo "<br /><br />";
+echo 'Hello, I\'m ' . $name . ', I\'m a ' . $position . ' please check my github link <br/>' . $github_url;
+echo "<br /><br />";
+echo 'Hello, I\'m ', $name, ', I\'m a ', $position, ' please check my github link <br/>', $github_url;
+
+?>
