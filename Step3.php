5aca6d99d728b2a0c7bbf383d5230914a09a0902
Mostafa Kassem <moustafakassem12@gmail.com>
2021-05-22 19:06:02 +0300

    Step 3

---
+++ Step3.php
@@ -0,0 +1,14 @@
+<?php
+
+$name='Mostafa';
+$email='mostafa.kassem.lb@gmail.com';
+
+$heigth=9;
+$width=9;
+$area=$heigth*$width;
+
+echo "My Name is $name and My Email is $email" ;
+echo "<br>";
+echo $area;
+
+?>
