a67350a331b0653fb217ee22205270ceb899a440
Mostafa Kassem <moustafakassem12@gmail.com>
2021-05-26 14:55:46 +0300

    Part 3

---
+++ Exercise-01/Part 3/output.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <link rel='stylesheet' href='style.css' />
+</head>
+<body class='body'>
+    <div>
+        <?php
+$check = $_POST["name"];
+$str1 = strrev($check);
+if ($str1 === $check) {
+    echo 'it is palindrome';
+} else {
+    echo 'is not palindrom <br /> <br /> <br />' . $check . '  !=  ' . $str1;
+}
+?>
+<br>
+    </div>
+</body>
+</html>
